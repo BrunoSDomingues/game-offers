b6ea09f3d5e240c7867c363b266e1eaf045347d7
BrunoSDomingues <brunao.signorelli@hotmail.com>
2019-10-23 16:08:01 -0300

    Requests 100% funcionais

--- backend/routes/api/get-data.js
+++ backend/routes/api/get-data.js
@@ -1,43 +1,135 @@
-const fetch = require("node-fetch");
-
-// Deals
+const http = require("http");
+// const isEmpty = require("is-empty");
 
 const url_deals = new URL("http://www.cheapshark.com/api/1.0/deals");
-
-function getDeals(params){
-    Object.keys(params).forEach(key => url_deals.searchParams.append(key, params[key]))
-    
-    fetch(url_deals).then(data =>{return data.json()}).catch(err =>{console.log(err)})
-}
-
-function getDealInfo(id){
-    Object.keys(id).forEach(key => url_deals.searchParams.append(key, id[key]))
-    
-    fetch(url_deals).then(data =>{return data.json()}).catch(err =>{console.log(err)})
-}
-
-// Games
-
 const url_games = new URL("http://www.cheapshark.com/api/1.0/games");
+const url_alerts = new URL("http://www.cheapshark.com/api/1.0/alerts");
 
-function getGamesList(params){
-    Object.keys(params).forEach(key => url_games.searchParams.append(key, params[key]))
-    
-    fetch(url_games).then(data =>{return data.json()}).catch(err =>{console.log(err)})
+function containsEncodedComponents(x) {
+    return (x !== decodeURIComponent(x));
 }
 
-function getGameInfo(id){
-    Object.keys(id).forEach(key => url_games.searchParams.append(key, id[key]))
-    
-    fetch(url_games).then(data =>{return data.json()}).catch(err =>{console.log(err)})
+// Gets ALL JSON outputs of requests
+
+function getJSON(url, params) {
+    Object.keys(params).forEach(key => {
+        if (containsEncodedComponents(params[key])){
+            while (params[key] !== decodeURIComponent(params[key])){
+                params[key] = decodeURIComponent(params[key])
+            }
+        }
+        url.searchParams.append(key, params[key])
+    })
+
+    return new Promise((resolve, reject) => {
+        http.get(url, (res) => {
+            let { statusCode } = res;
+            let contentType = res.headers['content-type'];
+
+            let error;
+
+            if (statusCode !== 200) {
+                error = new Error("Request failed.\n" + `Status Code: ${statusCode}`);
+            } else if (!/^application\/json/.test(contentType)) {
+                error = new Error('Invalid content-type.\n' +
+                    `Expected application/json but received ${contentType}`);
+            }
+
+            if (error) {
+                console.error(error.message);
+                res.resume();
+            }
+
+            res.setEncoding('utf-8');
+            let rawData = '';
+
+            res.on('data', (chunk) => {
+                rawData += chunk;
+            });
+
+            res.on('end', () => {
+                try {
+                    const parsedData = JSON.parse(rawData);
+                    resolve(parsedData);
+                } catch (e) {
+                    reject(e.message);
+                }
+            });
+        }).on('error', (e) => {
+            reject(`Got error ${e.message}`)
+        });
+    })
 }
 
-// Alerts
+// Example:
 
-function controlAlerts(params){
-    let url_alerts = "http://www.cheapshark.com/api/1.0/alerts";
+// let p = { id: "X8sebHhbc1Ga0dTkgg59WgyM506af9oNZZJLU9uSrX8%25253D" };
+// getJSON(url_deals, p)
+//     .then(resp => {
+//         if (!isEmpty(resp)) {
+//             console.log(resp)
+//         } else { 
+//             console.log("Request retornou vazio")
+//         }
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
 
-    Object.keys(params).forEach(key => url_alerts.searchParams.append(key, params[key]))
+// Returns if the alert was successfully set/deleted
 
-    fetch(url_alerts).then(data => {return data}).catch(err => {console.log(err)})
+function controlAlert(url, params) {
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+
+    return new Promise((resolve, reject) => {
+        http.get(url, (res) => {
+            let { statusCode } = res;
+            let contentType = res.headers['content-type'];
+
+            let error;
+
+            if (statusCode !== 200) {
+                error = new Error("Request failed.\n" + `Status Code: ${statusCode}`);
+            } else if (!/^text\/html/.test(contentType)) {
+                error = new Error('Invalid content-type.\n' +
+                    `Expected text/html but received ${contentType}`);
+            }
+
+            if (error) {
+                console.error(error.message);
+                res.resume();
+            }
+
+            res.setEncoding('utf-8');
+            let rawData = '';
+
+            res.on('data', (chunk) => {
+                rawData += chunk;
+            });
+
+            res.on('end', () => {
+                try {
+                    resolve(rawData);
+                } catch (e) {
+                    reject(e.message);
+                }
+            });
+        }).on('error', (e) => {
+            reject(`Got error ${e.message}`)
+        });
+    })
 }
+
+// Example:
+
+// let params = { action: "set", email: "test@test.com", gameID: 130, price: 14.99 };
+
+// controlAlert(url_alerts, params)
+//     .then(resp => {
+//         if (!isEmpty(resp)) {
+//             console.log(resp)
+//         } else { console.log("Request retornou vazio") }
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })

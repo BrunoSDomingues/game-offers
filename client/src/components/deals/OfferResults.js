b23017972bc1db4489cdb1754f369fb8fef2e2f2
BrunoSDomingues <brunao.signorelli@hotmail.com>
2019-11-02 02:35:13 -0300

    Resultados mostrando corretamente

--- client/src/components/deals/OfferResults.js
+++ client/src/components/deals/OfferResults.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import { stores } from "../stores";
 const fetch = require('node-fetch');
 
 const url_deals = new URL('http://www.cheapshark.com/api/1.0/deals');
@@ -23,42 +24,78 @@ class OfferResults extends Component {
     async componentDidMount() {
         const response = await fetch(url_deals);
         const data = await response.json();
-        this.setState({output: data, isLoading: false});
+        this.setState({ output: data, isLoading: false });
+    }
+
+    getRedirect(deal_id) {
+        let redir = new URL("http://www.cheapshark.com/redirect?");
+        let params = { dealID: deal_id };
+
+        Object.keys(params).forEach(key => {
+            if (params[key] !== decodeURIComponent(params[key])) {
+                while (params[key] !== decodeURIComponent(params[key])) {
+                    params[key] = decodeURIComponent(params[key])
+                }
+            }
+            redir.searchParams.append(key, params[key])
+        })
+
+        return redir;
+    }
+
+    createResults() {
+        let res = [];
+        let len = Object.keys(this.state.output).length;
+
+        for (let i = 0; i < len; i++) {
+            let r = this.state.output[i];
+            let store_info = stores[r.storeID];
+            let logo = new URL("http://www.cheapshark.com" + store_info.images.logo)
+            res.push(
+                <div className="result" key={i}>
+                    <div className="game">
+                        <div className="thumbnail">
+                            <img src={r.thumb} alt="" />
+                        </div>
+                        <div className="game-title">
+                            {r.title}
+                        </div>
+                    </div>
+                    <div className="prices">
+                        <div className="store">
+                            <img src={logo} style={{ maxWidth: "15%" }} />
+                            <a href={this.getRedirect(r.dealID)}>{store_info.storeName}</a>
+                        </div>
+                        <div className="price">
+                            <h7>US$ {r.salePrice}</h7>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+
+        return res;
     }
 
     render() {
         return (
-            <div style={{ height: "85vh" }} className="container valign-wrapper">
-                <div className="row">
-                            <Link to="/dashboard" className="btn-flat waves-effect">
-                                <i className="material-icons left">keyboard_backspace</i> Voltar à página principal
+            <div style={{ height: "100%" }} className="container valign-wrapper">
+                <div classNameName="row">
+                    <Link to="/dashboard" className="btn-flat waves-effect">
+                        <i className="material-icons left">keyboard_backspace</i> Voltar à página principal
                             </Link>
+                    <br/>
+                    <br/>
+                    <br/>
                     {this.state.isLoading || !this.state.output ? (
-                        <div className="row">
+                        <div classNameName="row">
                             <h5>Carregando...</h5>
                         </div>
                     ) : (
-                        <div id="results" style={{ width: "100%"}}>
-                            <div class="result">
-                                <div class="game">
-                                    <div class="thumbnail">
-                                        <img src={this.state.output[0].thumb}/>
-                                    </div>
-                                    <div class="game-title">
-                                        {this.state.output[0].title}
-                                    </div>
-                                </div>
-                                <div class="prices">
-                                    <table>
-                                        <tr class="price-line">
-                                            <td class="store"></td>
-                                            <td class="price"></td>
-                                        </tr>
-                                    </table>
-                                </div>
+                            <div id="results" style={{ width: "100%" }}>
+                                {this.createResults()}
                             </div>
-                        </div>
-                    )}
+                        )}
                 </div>
             </div>
         )
